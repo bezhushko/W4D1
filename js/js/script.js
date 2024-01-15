c160f1dc55c09a15a49324f54c4d0da82caa2a83
bezhushko <151675558+bezhushko@users.noreply.github.com>
2024-01-15 18:48:52 +0100

    ok

---
+++ js/js/script.js
@@ -0,0 +1,74 @@
+/*
+PARTE 1: 
+Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
+Il tuo compito è creare una funzione che cercherà per posizione lavorativa E posizione geografica. Questi due valori verranno passati come parametri
+Ti abbiamo fornito un array chiamato "jobs" in fondo al file, NON modificarlo in alcun modo.
+L'algoritmo che devi realizzare cercherà SIA per posizione lavorativa che per posizione geografica.
+Prendi queste tre inserzioni ad esempio:
+
+      job1:  location: "NY, US",     title: "java dev"
+      job2:  location: "Genoa, IT"   title: "web dev"
+      job3:  location: "US"      title: "dev"
+
+Cercando contemporaneamente come posizione lavorativa "dev" e posizione geografica "US", dovresti ottenere come risultato solamente job1 e job3,
+in quanto job2 non soddisfa la condizione posta sulla posizione geografica.
+
+REQUISITI:
+- il tuo algoritmo deve tornare i risultati nella seguente forma:
+{
+  result: [], <-- inserisci qui le inserzioni che rispecchiano la posizione lavorativa e la posizione geografica richiesta
+  count: 0 <-- inserisci qui il numero totale delle inserzioni trovate
+}
+
+- la tua ricerca deve essere "case insensitive" (non deve essere influenzata da lettere maiuscole o minuscole nelle parole cercate). Questo e' possibile trasformando tutto in lettere minuscole con .toLowerCase()
+
+
+PARTE 2: 
+Nella pagina HTML, inserisci 2 input di tipo testo (uno per la location e uno per il titolo lavorativo, ricordati di diversificarli con un id) e un bottone con valore “cerca”
+
+Al click del bottone, il codice deve raccogliere i valori dei due input e darli in pasto alla funzione che hai creato nella parte 1. 
+
+Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati sulla pagina: 
+    Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
+    Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
+    SUGGERIMENTO: ti servira’ un ciclo for!
+
+*/
+
+
+  
+  function serachJob(title, location){
+    title= title.toLowerCase();
+    location=location.toLowerCase();
+
+    const result= [];
+    jobs.forEach(job => {
+        const jobTitle= job.title.toLowerCase();
+        const jobLocation= job.location.toLowerCase();
+        if (jobTitle.includes(title)&& jobLocation.includes(location)){
+            result.push(job)
+        }
+    });
+    return{
+        count: result.length,
+        result
+
+    }
+  }
+
+
+function search() {
+    const title = document.getElementById("searchJob").value;
+    const location = document.getElementById("searchPlace").value;
+
+    const searchResult = searchJobs( title, location)
+    console.log(searchResult)
+    return searchResult
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    // search()
+    showResults(search())
+})
